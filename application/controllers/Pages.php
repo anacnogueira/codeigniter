d92329db8899f849053ecc2da9b6a5449c8bc199
Ana Claudia <anacnogueira@gmail.com>
2018-04-05 16:41:38 -0300

    Inserindo registro

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -2,12 +2,18 @@
 
 class Pages extends CI_Controller
 {
+	public function __construct()
+	{
+
+		parent::__construct();
+		$this->load->model('pages_model');
+	}
+
 	public function index()
 	{
-		$this->load->model('pages_model');
+		
 
 		$pages = $this->pages_model->get();
-
 
 		$this->load->view('template/header');
 		$this->load->view('pages/index', ['pages' => $pages]);
@@ -17,5 +23,23 @@ class Pages extends CI_Controller
 	public function view($id)
 	{
 		echo 'Pages::view('.$id.')';
+	}
+
+	public function new()
+	{
+		$this->load->library('form_validation');
+
+		$this->form_validation->set_rules('title', 'Título', 'required');
+		$this->form_validation->set_rules('body', 'Conteúdo', 'required');
+
+		if (!$this->form_validation->run()) {
+			$this->load->view('template/header');
+			$this->load->view('pages/new');
+			$this->load->view('template/footer');
+		} else {
+			$data['back'] = '/pages';
+			 $this->pages_model->new();
+			 $this->load->view('template/success', $data);
+		}
 	}
 }
--- application/models/Pages_model.php
+++ application/models/Pages_model.php
@@ -2,12 +2,31 @@
 
 class Pages_model extends CI_Model
 {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+	}
+
 	public function get()
 	{
 
-		$this->load->database();
 		$query = $this->db->get('pages');
 
 		return $query->result();
+	}
+
+	public function new()
+	{
+		$this->load->helper('url');
+		$slug = url_title($this->input->post('title'), 'dash', true);
+
+		$data = [
+			'title' => $this->input->post('title'),
+			'body' => $this->input->post('body'),
+			'slug' => $slug
+		];
+
+		return $this->db->insert('pages', $data);
 	}
 }
